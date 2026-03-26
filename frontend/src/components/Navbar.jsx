47ace853c298a8d6b10460af477bb9f34d94b1ec
Himanshu Meshram <meshramhimanshu20@gmail.com>
2026-03-26 18:11:11 +0530

    Update navbar title from 'CipherSQLStudio' to 'SQLStudio'

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
             <div className="navbar__inner container">
                 <Link to="/" className="navbar__brand">
                     <span className="navbar__logo">⟨/⟩</span>
-                    <span className="navbar__title">CipherSQLStudio</span>
+                    <span className="navbar__title">SQLStudio</span>
                 </Link>
                 <div className="navbar__tagline">Interactive SQL Learning</div>
             </div>
